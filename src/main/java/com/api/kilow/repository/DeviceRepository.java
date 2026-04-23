317ec9c50dfa22e9d6717ffc9f9ed9e377f05662
Maria Eduarda Salomão <maria.salomao.1523618@sga.pucminas.br>
2026-04-23 16:50:09 -0300

    Ajuste na entidade e na interface de conexão desta com o banco

--- src/main/java/com/api/kilow/repository/DeviceRepository.java
+++ src/main/java/com/api/kilow/repository/DeviceRepository.java
@@ -2,10 +2,9 @@ package com.api.kilow.repository;
 
 import com.api.kilow.model.Device;
 import com.api.kilow.model.User;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface DeviceRepository extends JpaRepository<Device, Long> {
-    Optional<Device> findByUser(User user);
+  List<Device> findByUser(User user);
 }
