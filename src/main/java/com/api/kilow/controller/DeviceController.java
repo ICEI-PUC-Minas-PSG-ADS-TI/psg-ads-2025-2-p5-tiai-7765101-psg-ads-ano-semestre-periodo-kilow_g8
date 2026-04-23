74e3cdf36e9b028d4cf79b2b35c9e0642bebdc25
Maria Eduarda Salomão <maria.salomao.1523618@sga.pucminas.br>
2026-04-23 16:50:29 -0300

    Ajuste controller

--- src/main/java/com/api/kilow/controller/DeviceController.java
+++ src/main/java/com/api/kilow/controller/DeviceController.java
@@ -1,49 +1,46 @@
 package com.api.kilow.controller;
 
-import com.api.kilow.dto.device.CreateDeviceRequestDTO;
-import com.api.kilow.dto.device.CreateDeviceResponseDTO;
-import com.api.kilow.dto.device.GetDeviceResponseDTO;
+import com.api.kilow.dto.device.*;
 import com.api.kilow.service.DeviceService;
 import jakarta.validation.Valid;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/devices")
 public class DeviceController {
 
-    @Autowired
-    DeviceService deviceService;
+  @Autowired DeviceService deviceService;
 
-    @PostMapping("/register")
-    public ResponseEntity<CreateDeviceResponseDTO> createDevice (@Valid @RequestBody CreateDeviceRequestDTO request){
-        CreateDeviceResponseDTO device = deviceService.createDevice(request);
+  @PostMapping("/register")
+  public ResponseEntity<CreateDeviceResponse> createDevice(
+      @Valid @RequestBody CreateDeviceRequest request) {
+    CreateDeviceResponse device = deviceService.createDevice(request);
 
-        return ResponseEntity.status(HttpStatus.CREATED).body(device);
-    }
+    return ResponseEntity.status(HttpStatus.CREATED).body(device);
+  }
 
-    @GetMapping("/getAll")
-    public ResponseEntity<List<GetDeviceResponseDTO>> getAllDevices(){
-        List<GetDeviceResponseDTO> allDevices = deviceService.getAllDevices();
+  @GetMapping("/getAll")
+  public ResponseEntity<List<GetDeviceResponse>> getAllDevices() {
+    List<GetDeviceResponse> allDevices = deviceService.getAllDevices();
 
-        return ResponseEntity.ok(allDevices);
-    }
+    return ResponseEntity.ok(allDevices);
+  }
 
-    @DeleteMapping("/delete/{id}")
-    public ResponseEntity<Void> deleteDevice(@PathVariable Long id){
-        deviceService.deleteDevice(id);
+  @DeleteMapping("/delete/{id}")
+  public ResponseEntity<Void> deleteDevice(@PathVariable Long id) {
+    deviceService.deleteDevice(id);
 
-        return ResponseEntity.noContent().build();
-    }
+    return ResponseEntity.noContent().build();
+  }
 
-    @PutMapping("/update/{id}")
-    public ResponseEntity<CreateDeviceResponseDTO> update(@PathVariable Long id, @Valid @RequestBody CreateDeviceRequestDTO bodyRequest){
-        CreateDeviceResponseDTO updatedDevice = deviceService.updateDevice(bodyRequest, id);
-        return ResponseEntity.ok(updatedDevice);
-    }
-
+  @PutMapping("/update/{id}")
+  public ResponseEntity<UpdateDeviceResponse> update(
+      @PathVariable Long id, @Valid @RequestBody UpdateDeviceRequest request) {
+    UpdateDeviceResponse updatedDevice = deviceService.updateDevice(request, id);
+    return ResponseEntity.ok(updatedDevice);
+  }
 }
