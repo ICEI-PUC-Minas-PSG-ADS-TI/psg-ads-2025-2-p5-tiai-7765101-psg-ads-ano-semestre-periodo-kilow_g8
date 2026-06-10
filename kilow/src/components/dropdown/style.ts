41b6481894562425880a764c94c8ab67fe497756
Maria Eduarda Salomão <maria.salomao.1523618@sga.pucminas.br>
2026-06-09 22:48:03 -0300

    Criação de componente de dropdown

---
+++ kilow/src/components/dropdown/style.ts
@@ -0,0 +1,73 @@
+import styled from 'styled-components';
+import { colors } from '../theme';
+
+interface InputTriggerProps {
+  isSelected: boolean;
+}
+
+export const InputTrigger = styled.button<InputTriggerProps>`
+  width: 100%;
+  height: 36px;
+  padding: 8px 12px;
+  background-color: ${colors.lightGray};
+  border-radius: 12px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  cursor: pointer;
+  font-size: 16px;
+  text-align: left;
+  transition: border-bottom-color 0.2s ease;
+  ${({ isSelected }) =>
+    isSelected
+      ? ` 
+      outline: none;
+      border-bottom: 2px solid ${colors.cyanBlue};`
+      : `border: 1px solid ${colors.lightGray};
+`}
+
+  &[data-placeholder] {
+    color: ${colors.darkTextGray}80;
+  }
+`;
+
+export const DropdownItem = styled.li`
+  padding: 8px 12px;
+  color: ${colors.white};
+  font-size: 12px;
+  cursor: pointer;
+  transition: background-color 0.2s ease;
+  list-style: none;
+
+  &[data-highlighted] {
+    background-color: ${colors.cyanBlue}25;
+  }
+
+  &[data-selected] {
+    background-color: ${colors.lightGray}25;
+    font-weight: bold;
+  }
+`;
+
+export const DropdownPopup = styled.div`
+  background-color: ${colors.darkBlue};
+  border-radius: 4px;
+  padding: 0;
+  width: var(--anchor-width);
+  max-height: 160px;
+  overflow-y: auto;
+  box-shadow: 0px 8px 16px ${colors.black}25;
+  z-index: 50;
+  outline: none;
+
+  &::-webkit-scrollbar {
+    width: 6px;
+  }
+  &::-webkit-scrollbar-track {
+    background: transparent;
+  }
+  &::-webkit-scrollbar-thumb {
+    background-color: ${colors.cyanBlue}80;
+    border-radius: 4px;
+  }
+`;
