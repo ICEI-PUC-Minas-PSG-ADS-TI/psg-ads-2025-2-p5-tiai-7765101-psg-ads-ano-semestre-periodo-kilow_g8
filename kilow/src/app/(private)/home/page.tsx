c940f9cc9133a3b6209a6dc8fbb454356aff0ba4
Maria Eduarda Salomão <maria.salomao.1523618@sga.pucminas.br>
2026-06-09 22:29:01 -0300

    Revert "Estruturação de rotas privadas e tela provisória de home"
    This reverts commit 2b21f70fa343d95db10ad9ba69088dd5bbb24f84.

--- kilow/src/app/(private)/home/page.tsx
+++ kilow/src/app/(private)/home/page.tsx
@@ -1,26 +1,23 @@
-'use client';
-import { logoutAction } from '@/actions/services/auth';
-import Button from '@/components/button';
-import { StyledText } from '@/components/text';
+import { logoutAction } from '@/actions/auth';
+import { StyledText } from '@/app/(public)/text';
+import Header from '@/components/header';
 import { colors } from '@/components/theme';
 import Image from 'next/image';
-import { HomeContainer } from './styles';
 
 const HomePage = () => {
   return (
-    <HomeContainer>
-      <div
-        style={{
-          justifyContent: 'center',
-          margin: 'auto',
-        }}
-      >
-        <StyledText
-          color="black"
-          size={32}
-          weight={300}
-          style={{ textAlign: 'center' }}
-        >
+    <div
+      style={{
+        display: 'flex',
+        minHeight: '100vh',
+        width: '100vw',
+        flexDirection: 'column',
+        gap: 12,
+      }}
+    >
+      <Header />
+      <div style={{ justifyContent: 'center', margin: 'auto' }}>
+        <StyledText color="black" size={32} weight={300}>
           Estamos trabalhando nos seus dados ;)
         </StyledText>
         <Image
@@ -29,16 +26,23 @@ const HomePage = () => {
           height={500}
           alt="homeImage"
         />
-        <Button
+        <button
           onClick={logoutAction}
-          variant="yellow"
-          size="large"
-          style={{ width: '100%' }}
+          style={{
+            backgroundColor: colors.yellow,
+            color: colors.darkBlue,
+            fontSize: 28,
+            padding: 8,
+            borderRadius: 12,
+            border: 0,
+            marginBlock: 12,
+            width: '100%',
+          }}
         >
           Loggout
-        </Button>
+        </button>
       </div>
-    </HomeContainer>
+    </div>
   );
 };
 
